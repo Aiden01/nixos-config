38bde57ac880803d6bab13d3524807c33f7cdc71
aiden01 <liangu@protonmail.com>
2019-07-10 13:42:54 -0400

    set up home-manager

---
+++ home.nix
@@ -0,0 +1,52 @@
+{ pkgs, ... }:
+
+{
+  # Packages
+  home.packages = with pkgs; [
+    wget
+    vim
+    git
+    #emacs
+    firefox
+    zsh
+    discord
+    oh-my-zsh
+    ghc
+    stack
+    cabal-install
+  ];
+
+  # Packages configuration
+  programs = {
+
+    home-manager = {
+     enable = true;
+     path = "https://github.com/rycee/home-manager/archive/release-19.03.tar.gz";
+    };
+
+    git = {
+      enable = true;
+      userName = "aiden01";
+      userEmail = "liangu@protonmail.com";
+    };
+
+    zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["git" "sudo"];
+        theme = "lambda";
+      };
+    };
+
+    # emacs
+    #emacs = {
+    #  enable = true;
+    #  extraPackages = epkgs: [
+    #    epkgs.nix-mode
+    #    epkgs.elcord
+    #    epkgs.haskell-mode
+    #  ];
+    #};
+  };
+}
