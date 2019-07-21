cf0ac9eb911f0ce68303400ceafe455eccaac7dd
aiden01 <liangu@protonmail.com>
2019-07-20 22:13:06 -0400

    Add i3 module

---
+++ modules/i3.nix
@@ -0,0 +1,30 @@
+{config, lib, pkgs, ...}:
+
+with lib;
+
+let
+  cfg = config.customModules.i3;
+in {
+  options.customModules.i3 = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Wether to enable xmonad";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver.enable = true;
+    services.xserver.windowManager.i3 = {
+      enable = true;
+      # packages to be installed with i3
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+      ];
+    };
+    services.xserver.windowManager.default = "i3";
+  };
+}
